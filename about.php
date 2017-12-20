9ea19ed1f68cfc163138024cf7b6c7b48ee97d02
djkolt <djkolt@mail.ru>
2017-12-20 21:53:39 +0300

    about php

---
+++ about.php
@@ -0,0 +1,222 @@
+
+
+<?php include('../secondary_top.php'); ?>
+
+<section class="secondary">
+    <div style="height: 100px;">
+    </div>
+    <header class="section main">
+        <div class="inwards">
+            <div class="padding">
+                <h1>Блог</h1>
+            </div>
+        </div>
+    </header>
+
+    <?php include('../secondary_blog.php'); ?>
+
+    <section class="blog-about">
+        <div class="blog-logo"><img src="/bitrix/templates/beats/img/blog-logo.jpg" style="width: 100%;"></div>
+        <!--<div class="blog-logo-arrow"></div>-->
+
+        <div class="blog-about-information">
+
+            <header class="section main" style="background-color: white; padding-bottom: 0px; padding-top: 20px;">
+                <div class="inwards">
+                    <div class="padding">
+                        <h2 style="margin-bottom: 30px;">Давайте знакомиться!</h2>
+                        <p style="margin-bottom: 0px; text-transform: uppercase; font-family: helvetica-thin;" class="about-me subheader">Профессиональный опыт и немного личного</p>
+                    </div>
+                </div>
+            </header>
+            <div class="inwards about-me text" style="padding-bottom: 80px;">
+                <p><b>Дата рождения:</b> 27 марта 1981 г.</p>
+                <br>
+                <p><b>Рост:</b> 188 см</p>
+                <br>
+                <p><b>Кратко и скромно:</b> Интеллигентный эстет-перфекционист в 3 поколении с привлекательной внешностью</p>
+                <br>
+                <p><b>Особые приметы:</b> притягательная улыбка.</p>
+                <br>
+				<p><b>Увлечения:</b> высокая кулинария, йога, большой теннис, путешествия и фотосъемка.</p>
+				<br>
+                <p><b>Конфиденциально:</b> Женат</p>
+            </div>
+            <div class="arrow-down"></div>
+        </div>
+
+        <div class="about-timeline">
+
+            <header class="section main">
+                <div class="inwards">
+                    <div class="padding">
+                        <h2>Профессиональная деятельность</h2>
+                    </div>
+                </div>
+            </header>
+
+            <div class="about-timeline-group">
+
+               <div class="about-timeline-item">
+
+                    <div class="about-timeline-item-image" style="background: url(/img/timeline-1.png) white center center no-repeat;">
+
+                    </div>
+
+                    <div class="about-timeline-item-text about-timeline-item-text-left">
+                        <div class="about-timeline-item-text-wrapper">
+                            Преподаватель, Санкт-Петербургский Государственный<br>
+                            Университет Культуры и Искусств, кафедра режиссуры<br>
+                            театрализованных представлений и праздников
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="about-timeline-pipe"></div>
+
+                <div class="about-timeline-item">
+
+                    <div class="about-timeline-item-image" style="background: url(/img/timeline-2.png) white center center no-repeat;">
+
+                    </div>
+
+                    <div class="about-timeline-item-text about-timeline-item-text-right">
+                        <div class="about-timeline-item-text-wrapper">
+                            Радиостанция Динамит-ФМ - диджей
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="about-timeline-pipe"></div>
+
+                <div class="about-timeline-item">
+
+                    <div class="about-timeline-item-image" style="background: url(/img/timeline-3.png) white center center no-repeat;">
+
+                    </div>
+
+                    <div class="about-timeline-item-text about-timeline-item-text-left">
+                        <div class="about-timeline-item-text-wrapper">
+                            Санкт-Петербургский Государственный<br>
+                            Драматический Театр "Приют Комедианта"
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="about-timeline-pipe"></div>
+
+                <div class="about-timeline-item">
+
+                    <div class="about-timeline-item-image" style="background: url(/img/timeline-4.png) white center center no-repeat;">
+
+                    </div>
+
+                    <div class="about-timeline-item-text about-timeline-item-text-right">
+                        <div class="about-timeline-item-text-wrapper">
+                            Продюсерская компания "Нева-Арт"
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="about-timeline-pipe"></div>
+                
+                <div class="about-timeline-more">
+                    Узнать<br>
+                    больше
+                </div>
+
+            </div>
+
+            <div class="about-timeline-group" style="display: none;">
+
+                <div class="about-timeline-item">
+
+                    <div class="about-timeline-item-image" style="background: url(/img/timeline-5.png) white center center no-repeat;">
+
+                    </div>
+
+                    <div class="about-timeline-item-text about-timeline-item-text-left">
+                        <div class="about-timeline-item-text-wrapper">
+                            Ведущий общегородского праздника<br>
+                            "Караоке по-петербургски",<br>
+                            Исаакиевская площадь, СПБ
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="about-timeline-pipe"></div>
+
+                <div class="about-timeline-item">
+
+                    <div class="about-timeline-item-image" style="background: url(/img/timeline-6.png) white center center no-repeat;">
+
+                    </div>
+
+                    <div class="about-timeline-item-text about-timeline-item-text-right">
+                        <div class="about-timeline-item-text-wrapper">
+                            Ведущий и режиссер праздника, посвященного<br>
+                            Дню Государственного Флага Российской Федерации,<br>
+                            Петропавловская крепость, Исаакиевская площадь, СПБ
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="about-timeline-pipe"></div>
+
+                <div class="about-timeline-item">
+
+                    <div class="about-timeline-item-image" style="background: url(/img/timeline-7.png) white center center no-repeat;">
+
+                    </div>
+
+                    <div class="about-timeline-item-text about-timeline-item-text-left">
+                        <div class="about-timeline-item-text-wrapper">
+                            Победитель телевизионного проекта<br>
+                            "Четыре свадьбы" на канале РЕН-ТВ
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="about-timeline-pipe"></div>
+
+                <div class="about-timeline-item">
+
+                    <div class="about-timeline-item-image" style="background: url(/img/timeline-8.png) white center center no-repeat;">
+
+                    </div>
+
+                    <div class="about-timeline-item-text about-timeline-item-text-right">
+                        <div class="about-timeline-item-text-wrapper">
+                            Текст
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="about-timeline-pipe"></div>
+                
+                <div class="about-timeline-end">
+                    И многие другие<br>
+                    проекты<br>...
+                </div>
+
+            </div>
+
+        </div>
+        
+    </section>
+
+</section>
+
+<?php include('../secondary_bottom.php'); ?>
+<script src="<?=SITE_TEMPLATE_PATH?>/js/nav-menus.js"></script>
+<script src="<?=SITE_TEMPLATE_PATH?>/js/about-blog.js"></script>
+<script src="<?=SITE_TEMPLATE_PATH?>/js/blog.js"></script>
+
